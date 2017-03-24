7e1bb80bbc4291436c5c796a4b9007c0d2f49e85
kaaniboy <kaanaksoyaz@gmail.com>
2017-03-24 12:32:44 -0700

    Attempt to connect to MongoDB

--- server.py
+++ server.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from pymongo import MongoClient
+
 app = Flask(__name__)
+
+client = None
 
 @app.route("/")
 def main():
     return "HII-C MongoDB Interface"
 
 if __name__ == "__main__":
+    client = MongoDB('mongodb://localhost:27017/')
     app.run()
