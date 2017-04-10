dd0180011705b3cafc157f004dc8ecf8e26904a6
kaaniboy <kaanaksoyaz@gmail.com>
2017-04-09 17:56:17 -0700

    Added validation to POST requests and checks for duplicate hashes

--- loupe_query.py
+++ loupe_query.py
@@ -5,6 +5,9 @@ from helpers import loadMongoURL
 
 import json
 
+def validate_post(json):
+    return (json != None) and ('hash' in json) and ('output' in json)
+
 class LoupeQuery(Resource):
     def __init__(self):
         self.client = MongoClient(loadMongoURL())
@@ -20,9 +23,19 @@ class LoupeQuery(Resource):
         return json.dumps(res)
 
     def post(self):
-        body = request.get_json(force=True)
-        self.collection.insert_one(body)
+        json = request.get_json(force=True)
 
-        res = {'success': True, 'message': 'The loupe query was successfully inserted.'}
+        message = ''
+
+        if validate_post(json):
+            if self.collection.find_one({'hash': json['hash']}) != None:
+                message = 'A loupe query with the provided hash already exists.'
+            else:
+                self.collection.insert_one(json)
+                message = 'The loupe query was successfully inserted.'
+        else:
+            message = 'The POST request is malformed.'
+
+        res = {'success': True, 'message': message}
 
         return json.dumps(res)
