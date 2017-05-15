0d6adab61ef0d7d4594b60566c7132c4ccce06f6
amichne <austinmichne@Austins-MacBook-Pro.local>
2017-05-15 12:06:11 -0700

    formatting

--- token_check.py
+++ token_check.py
@@ -2,8 +2,6 @@ from flask_restful import Resource
 from flask import request
 from pymongo import MongoClient
 from helpers import loadMongoURL
-from auth import requires_auth
-import json
 import requests
 
 
@@ -15,7 +13,7 @@ class TokenCheck(Resource):
     def post(self):
         code = request.form['code']
         redirect_uri = request.form['redirect_uri']
-        data = 'grant_type=authorization_code&code='+code+'&redirect_uri='+redirect_uri
+        data = 'grant_type=authorization_code&code=' + code + '&redirect_uri=' + redirect_uri
         headers = {'Content-Type': 'application/x-www-form-urlencoded'}
         client = '160324a1-e8fa-440c-8068-0f482701f1e8'
         client_secret = 'HNjyMOK6SkdVFuilQSuORvQ8duqg4DD7yJ0weZE3Vfffsfl3nY9JLOQewg-arYxSlnLkDh9c3Ldj5hfUQ2zUoA'
