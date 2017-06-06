de0ba6976f71a4c60ed33e32ca439f2ca604dfc9
cmajorb <cmajorb@gmail.com>
2017-06-06 15:59:40 -0700

    Integrating with our own FHIR server

--- token_check.py
+++ token_check.py
@@ -17,7 +17,12 @@ class TokenCheck(Resource):
         token_url = request.form['token_url']
         data = 'grant_type=authorization_code&code=' + code + '&redirect_uri=' + redirect_uri
         headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-        client = '82b330f7-1186-4059-8c31-62dce4b18d77'
-        client_secret = 'AKgTeRtUGHrr3Eu7HF0fJf6DfbrLTk2ieyeSpR0KUweizQWfqZICOYjHjUbXegMajNhTgQEKt5AZryb_5KAZTIs'
+        if token_url == 'http://id.healthcreek.org/token':
+            client = 'e848c885-be81-45e3-a441-14ab1cc6ad55'
+            client_secret = 'AfqgsKVOtE2VcasgXUZu4M_JCkNee1yvkaryDsjSoyoxQRPqmQrgyHePppPn2oAEg-BJvxKgQErYnHZaeYLUiA'
+        else:
+            client = '82b330f7-1186-4059-8c31-62dce4b18d77'
+            client_secret = 'AKgTeRtUGHrr3Eu7HF0fJf6DfbrLTk2ieyeSpR0KUweizQWfqZICOYjHjUbXegMajNhTgQEKt5AZryb_5KAZTIs'
+
         res = requests.post(token_url, headers=headers, auth=(client, client_secret), data=data)
         return res.json()
